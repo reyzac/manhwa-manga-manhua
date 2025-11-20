d8e963e98ff7786758fe5d42ef081fbd28b50fe8
caesare <reyzac@users.noreply.github.com>
2025-11-20 10:30:55 +0000

    create app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_manhwa():
-   return "Hello Manhwa!"
+   return "Hello Manhwa-manga!"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
